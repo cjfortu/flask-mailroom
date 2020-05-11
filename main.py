a8d05e0c87de8406492f10e3717c281591281c57
Clemente Fortuna <cjfortu@icloud.com>
2020-05-10 23:03:51 -0400

    Added all optional features, including registering a new donor with password.

--- main.py
+++ main.py
@@ -2,22 +2,109 @@ import os
 import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
+from passlib.hash import pbkdf2_sha256
 
-from model import Donation 
+from model import Donor, Donation
 
 app = Flask(__name__)
+# app.secret_key = b'\x9d\xb1u\x08%\xe0\xd0p\x9bEL\xf8JC\xa3\xf4J(hAh\xa4\xcdw\x12S*,u\xec\xb8\xb8'
+app.secret_key = os.environ.get('SECRET_KEY').encode()
+# app.secret_key = base64.b32encode(os.urandom(8)).decode().strip("=")
+
 
 @app.route('/')
 def home():
     return redirect(url_for('all'))
 
+
 @app.route('/donations/')
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    """
+    Create a new donation based on the session user.
+    """
+    if request.method == 'GET':
+        return render_template('create.jinja2', donor=session['donorname'])
+
+    if request.method == 'POST':
+        donor = Donor.get(Donor.name == session['donorname'])
+        donation = Donation(value=request.form['donation'], donor=donor)
+        donation.save()
+        return redirect(url_for('home'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """
+    Login for an existing donor, then redirect to creating a new donation.
+    """
+    if request.method == 'POST':
+        name = request.form['name']
+        password = request.form['password']
+        try:
+            donor = Donor.get(Donor.name == name)
+            # user = User.select().where(User.name == name).get()
+        except Donor.DoesNotExist:
+            return render_template('login.jinja2', error='Donorname or Password incorrect.')
+        else:
+            if pbkdf2_sha256.verify(password, donor.password):
+                session['donorname'] = donor.name
+                return redirect(url_for('create'))
+            else:
+                return render_template('login.jinja2', error='Donorname or Password incorrect.')
+
+    else:
+        return render_template('login.jinja2')
+
+
+@app.route('/new_donor', methods=['GET', 'POST'])
+def new_donor():
+    """
+    Add a new donor with password, then redirect to creating a new donation.
+    """
+    names = [donor.name for donor in Donor.select()]
+
+    if request.method == 'POST':
+        name = request.form['name']
+        password = request.form['password']
+        if name in names:
+            return render_template('new_donor.jinja2', error='Donor already exists.')
+        else:
+            donor = Donor(name=name, password=pbkdf2_sha256.hash(password))
+            donor.save()
+            session['donorname'] = donor.name
+            return redirect(url_for('create'))
+
+    else:
+        return render_template('new_donor.jinja2')
+
+
+@app.route('/donor_alls', methods=['GET', 'POST'])
+def donor_alls():
+    """
+    Search a donor and display all their donations.
+    """
+    if request.method == 'GET':
+        return render_template('donor_alls.jinja2')
+
+    if request.method == 'POST':
+        try:
+            donor = Donor.get(Donor.name == request.form['name'])
+        except Donor.DoesNotExist:
+            return render_template('donor_alls.jinja2', error='Donor does not exist.')
+        else:
+            query = Donation.select().where(Donation.donor == donor)
+            print('ok heres the query', query)
+            donations = [donation.value for donation in query]
+            print('ok heres the donations', donations)
+            return render_template('donor_alls.jinja2', donor=donor.name, donations=donations)
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
     app.run(host='0.0.0.0', port=port)
-
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import random
-
-from model import db, Donor, Donation 
+from passlib.hash import pbkdf2_sha256
+from model import db, Donor, Donation
 
 db.connect()
 
@@ -10,17 +10,16 @@ db.drop_tables([Donor, Donation])
 
 db.create_tables([Donor, Donation])
 
-alice = Donor(name="Alice")
+alice = Donor(name="Alice", password=pbkdf2_sha256.hash("alice1"))
 alice.save()
 
-bob = Donor(name="Bob")
+bob = Donor(name="Bob", password=pbkdf2_sha256.hash("bob2"))
 bob.save()
 
-charlie = Donor(name="Charlie")
+charlie = Donor(name="Charlie", password=pbkdf2_sha256.hash("charlie3"))
 charlie.save()
 
 donors = [alice, bob, charlie]
 
 for x in range(30):
-    Donation(donor=random.choice(donors), value=random.randint(100, 10000)).save()
-
+    Donation(donor=random.choice(donors), value=round(random.uniform(100, 10000), 2)).save()
